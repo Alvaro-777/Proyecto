32f5d666a8549f4a7c1e9ccf732bc4c0852c905b
Francisco Manuel Vigil <fruivig204@g.educaand.es>
2026-01-12 19:01:16 +0100

    cambio de js para verificar login

--- OretanIA/public/script/signup.js
+++ OretanIA/public/script/signup.js
@@ -1,34 +1,73 @@
-
+const nombre = document.getElementById('signup-name');
+const apellidos = document.getElementById('signup-surname');
+const mail = document.getElementById('signup-email');
+const password = document.getElementById('signup-pswd');
+const confirm = document.getElementById('signup-confirm');
+const patternNombre = /^[a-zñáéíóú]+(\s[a-zñáéíóú]+)?$/i;
+const patternApellidos = /^[a-zñáéíóú]+(\s[a-zñáéíóú]+)*$/i;
+const patternMail = /^[\w.-]+@[\w.-]+\.[a-zA-Z]{2,}$/;
+const patternPwd = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]).{8,}$/;
 //Si la validación falla no recarga
 document.getElementById('signup-form')
-        .addEventListener('submit', (evt) => {
-    if(!validate()) {
-        //Que hacer si el formulario contiene datos inválidos
-        evt.preventDefault();
-    }
-});
+    .addEventListener('submit', (evt) => {
+        if (!validate()) {
+            //Que hacer si el formulario contiene datos inválidos
+            evt.preventDefault();
+        }
+    });
 
 document.querySelectorAll('input[type=text], input[type=email], input[type=password]')
-        .forEach((input) => {
-            input.addEventListener('focus', (evt) => {
-                document.getElementById(evt.target.id + '-error')
-                        .innerText = '';
-            });
-        })
+    .forEach((input) => {
+        input.addEventListener('focus', (evt) => {
+            document.getElementById(evt.target.id + '-error')
+                .innerText = '';
+        });
+    })
 ;
 
 function validate() {
     //Comprobar si los datos son validos en el cliente
-    let nombre = document.getElementById('signup-name').value;
-    let apellidos = document.getElementById('signup-surname').value;
-    let password = document.getElementById('signup-pswd').value;
-    let confirm = document.getElementById('signup-confirm').value;
 
-    if(password !== confirm){
+    if (password.value !== confirm.value) {
         document.getElementById("signup-confirm-error")
-                .innerText = "Las contraseñas suministradas no coinciden.";
+            .innerText = "Las contraseñas suministradas no coinciden.";
         return false;
     }
 
     return true;
 }
+
+nombre.addEventListener("input", (e) => {
+    const valor = e.target.value;
+    if (patternNombre.test(valor)) {
+        e.target.style.backgroundColor = "lightgreen";
+    } else {
+        e.target.style.backgroundColor = "lightcoral";
+    }
+});
+
+apellidos.addEventListener("input", (e) => {
+    const valor = e.target.value;
+    if (patternApellidos.test(valor)) {
+        e.target.style.backgroundColor = "lightgreen";
+    } else {
+        e.target.style.backgroundColor = "lightcoral";
+    }
+});
+mail.addEventListener("input", (e) => {
+    const valor = e.target.value;
+    if (patternMail.test(valor)) {
+        e.target.style.backgroundColor = "lightgreen";
+    } else {
+        e.target.style.backgroundColor = "lightcoral";
+    }
+});
+password.addEventListener("input", (e) => {
+    const valor = e.target.value;
+    if (patternPwd.test(valor)) {
+        e.target.style.backgroundColor = "lightgreen";
+    } else {
+        e.target.style.backgroundColor = "lightcoral";
+    }
+});
+
